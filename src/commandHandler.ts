33134b47c6a1e7b80d119636bffbc404a5a071c9
matievisthekat <matievisthekat@gmail.com>
2022-11-07 11:55:43 +0200

    formatting

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -10,23 +10,23 @@ export interface Command {
 export async function findCommands(baseDir: string): Promise<Command[]> {
 	const res: Command[] = [];
 
-  const dirents = fs.readdirSync(path.join(__dirname, 'commands', baseDir), {withFileTypes: true});
-  for (const dirent of dirents) {
-    const direntPath = path.join(baseDir, dirent.name);
+	const dirents = fs.readdirSync(path.join(__dirname, 'commands', baseDir), {withFileTypes: true});
+	for (const dirent of dirents) {
+		const direntPath = path.join(baseDir, dirent.name);
 
 		if (dirent.isDirectory()) {
-			res.push(... await findCommands(direntPath));
+			res.push(...(await findCommands(direntPath)));
 		} else if (dirent.isFile() && dirent.name.split('.').pop() === 'js') {
 			const filePath = path.join(__dirname, 'commands', baseDir, dirent.name);
 
-			const {default: mod} = await import(filePath)
-      if ('slash' in mod && 'execute' in mod) {
-        if (mod.slash instanceof SlashCommandBuilder && typeof mod.execute === 'function') {
-          res.push(mod);
-        }
-      }
+			const {default: mod} = await import(filePath);
+			if ('slash' in mod && 'execute' in mod) {
+				if (mod.slash instanceof SlashCommandBuilder && typeof mod.execute === 'function') {
+					res.push(mod);
+				}
+			}
 		}
-  }
+	}
 
 	return res;
 }
