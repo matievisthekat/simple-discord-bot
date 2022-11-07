2123aef69d78b843b6ca3924fbc2a732deb28d3b
matievisthekat <matievisthekat@gmail.com>
2022-11-07 13:55:25 +0200

    update client interface

---
+++ src/custom-typings.d.ts
@@ -0,0 +1,9 @@
+import * as discord from 'discord.js';
+import { Command } from './commandHandler';
+
+declare module 'discord.js' {
+  interface Client {
+    commands: discord.Collection<string, Command>;
+    sql: Sequelize;
+  }
+}
