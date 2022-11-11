518bae8ab93bf4c10aee6aa42f95b1f25f9b3819
matievisthekat <matievisthekat@gmail.com>
2022-11-11 13:52:54 +0200

    add shop command

---
+++ src/commands/Currency/shop.ts
@@ -0,0 +1,33 @@
+import {ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder} from 'discord.js';
+import ShopItems from '../../models/shopitems';
+
+export default {
+	slash: new SlashCommandBuilder().setName('shop').setDescription('View the items in the shop'),
+	async execute(int: ChatInputCommandInteraction) {
+		if (!int.guild) return;
+
+		const items = await ShopItems.findAll({
+			where: {
+				guild_id: int.guild.id
+			}
+		});
+
+		if (items.length === 0) {
+			await int.reply({content: "This server doesn't have any items in their shop"});
+			return;
+		}
+
+		const embed = new EmbedBuilder()
+			.setAuthor({name: int.guild.name, iconURL: int.guild.iconURL() || undefined})
+			.addFields(
+				items.map((i) => {
+					return {
+						name: `#${i.id}`,
+						value: `<@&${i.role_id}>`
+					};
+				})
+			);
+
+		await int.reply({embeds: [embed]});
+	}
+};
