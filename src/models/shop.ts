208449fdf04030ef43377f64702a94bd9f9608d8
matievisthekat <matievisthekat@gmail.com>
2022-11-11 13:35:02 +0200

    add id field

--- src/models/shop.ts
+++ src/models/shop.ts
@@ -1,6 +1,7 @@
 import { InferAttributes, InferCreationAttributes, Model } from "sequelize";
 
 export default class Shop extends Model<InferAttributes<Shop>, InferCreationAttributes<Shop>> {
+  declare id: number;
   declare guild_id: string;
   declare role_id: string;
   declare price: number;
